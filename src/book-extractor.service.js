88237fc0971d4c1b7fbf785dddaf216169500435
k0sen <caladien12@gmail.com>
2020-05-05 09:57:48 +0300

    add tests of transformJSONToBookObject function

--- src/book-extractor.service.js
+++ src/book-extractor.service.js
@@ -2,20 +2,21 @@ const makeBook = ({ bookRepository, readFile, parseXmlToJson, getValueFromObject
   const handleBook = async (bookPath) => {
     const bookContent = await readFile(bookPath);
     const parsedBookContent = await parseXmlToJson(bookContent);
-    await bookRepository.create(parseRdfToBook(parsedBookContent));
+    await bookRepository.create(transformJSONToBookObject(parsedBookContent));
   }
 
-  const parseRdfToBook = (content) => ({
-    title: getValueFromObject(content, 'rdf:RDF.pgterms:ebook.0.dcterms:title.0'),
-    author: getValueFromObject(content, 'rdf:RDF.pgterms:ebook.0.dcterms:creator.0.pgterms:agent.0.pgterms:alias.0'),
-    publisher: getValueFromObject(content, 'rdf:RDF.pgterms:ebook.0.dcterms:publisher.0'),
-    publicationDate: getValueFromObject(content, 'rdf:RDF.pgterms:ebook.0.dcterms:issued.0._'),
-    language: getValueFromObject(content, 'rdf:RDF.pgterms:ebook.0.dcterms:language.0.rdf:Description.0.rdf:value.0._'),
-    licenseRights: getValueFromObject(content, 'rdf:RDF.pgterms:ebook.0.dcterms:rights.0'),
+  const transformJSONToBookObject = jsonObject => ({
+    title: getValueFromObject(jsonObject, 'rdf:RDF.pgterms:ebook.0.dcterms:title.0'),
+    author: getValueFromObject(jsonObject, 'rdf:RDF.pgterms:ebook.0.dcterms:creator.0.pgterms:agent.0.pgterms:alias.0'),
+    publisher: getValueFromObject(jsonObject, 'rdf:RDF.pgterms:ebook.0.dcterms:publisher.0'),
+    publicationDate: getValueFromObject(jsonObject, 'rdf:RDF.pgterms:ebook.0.dcterms:issued.0._'),
+    language: getValueFromObject(jsonObject, 'rdf:RDF.pgterms:ebook.0.dcterms:language.0.rdf:Description.0.rdf:value.0._'),
+    licenseRights: getValueFromObject(jsonObject, 'rdf:RDF.pgterms:ebook.0.dcterms:rights.0'),
   });
 
   return {
     handleBook,
+    transformJSONToBookObject,
   }
 }
 
--- test/book-extractor.service/index.spec.js
+++ test/book-extractor.service/index.spec.js
@@ -6,6 +6,25 @@ const { parseXmlToJson } = require('../../src/xmlParser');
 const getValueFromObject = require('../../src/helpers/getValueFromObject');
 
 let fileWrited = false;
+const fileNames = ['pg1', 'pg2'];
+const properBookObjects = {
+  pg1: {
+    "title": "The Declaration of Independence of the United States of America",
+    "author": "United States President (1801-1809)",
+    "publisher": "Project Gutenberg",
+    "publicationDate": "1971-12-01",
+    "language": "en",
+    "licenseRights": "Public domain in the USA."
+  },
+  pg2: {
+    "title": "The United States Bill of Rights\r\nThe Ten Original Amendments to the Constitution of the United States",
+    "author": "U.S.A.",
+    "publisher": "Project Gutenberg",
+    "publicationDate": "1972-12-01",
+    "language": "en",
+    "licenseRights": "Public domain in the USA."
+  },
+}
 
 const mockedDependencies = {
   bookRepository: {
@@ -16,11 +35,21 @@ const mockedDependencies = {
   getValueFromObject,
 }
 
-const { handleBook } = makeBookExtractService(mockedDependencies);
+const { handleBook, transformJSONToBookObject } = makeBookExtractService(mockedDependencies);
 
 describe('Book extractor service', () => {
-  it('Should handle file parsing and saving', async () => {
-    const res = await handleBook(path.resolve(__dirname, `../xmlParser/books/pg1.rdf`));
-    expect(fileWrited).to.be.true;
+  it('Should transfrom JSON metadata to the book object', () => {
+    fileNames.forEach(async fileName => {
+      const jsonBookContent = await readFile(path.resolve(__dirname, `../xmlParser/booksOutput/${fileName}.json`));
+      const bookObject = transformJSONToBookObject(JSON.parse(jsonBookContent));
+      expect(bookObject).to.deep.equal(properBookObjects[fileName]);
+    });
+  });
+
+  it('Should handle file parsing and saving', () => {
+    fileNames.forEach(async fileName => {
+      await handleBook(path.resolve(__dirname, `../xmlParser/books/${fileName}.rdf`));
+      expect(fileWrited).to.be.true;
+    })
   });
 });
